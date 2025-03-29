c8fa4c9c82469dd3e43bfcde381926c39bddaa1d
zhaozixu <1306445936@qq.com>
2025-03-29 19:14:08 +0800

    新建了一个项目

---
+++ src/test/java/com/bank/duty/DutyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.duty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
